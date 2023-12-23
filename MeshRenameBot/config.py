ecc9d270df94dd20b0394b1d76a92a8583bb822e
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-12-23 06:46:50 +0530

    config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -5,9 +5,9 @@ try:
 except ImportError:
     class Config:
         DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        API_HASH = [str, "1f5343b0646645ca1eaf7c4759fc248f"]
+        API_ID = [int, 26376042]
+        BOT_TOKEN = [str, "6430230017:AAEzPGiVtyeFTuhwMhkcAz9m2PjJklg6mQY"]
         COMPLETED_STR = [str, "█"]
         REMAINING_STR = [str, "▒"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -16,14 +16,14 @@ except ImportError:
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[2036803347]]
+        OWNER_ID = [int, 2036803347]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"ANI_BOTS_UPDATES"]
+        FORCEJOIN_ID = [int,-1001918398170]
 
-        TRACE_CHANNEL = [int, 0]
+        TRACE_CHANNEL = [int, -1001918398170]
 
 try:
     from .tconfig import Commands
